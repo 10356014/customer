e53e5988b6b6c0bb9bd4429d4a15ea4e88c127f0
lulu <10356014@ntub.edu.tw>
2018-10-10 18:07:09 +0800

    顧客抽號UI完成

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,private alertCtrl: AlertController) {
   }
-
+  cutHair(){
+    let alert = this.alertCtrl.create({
+      title: '剪髮確認',
+      message: '您是16號',
+      buttons: [
+        {
+          text: '取消',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel');
+          }
+        },
+        {
+          text: '確認',
+          handler: () => {
+            console.log('OK');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
